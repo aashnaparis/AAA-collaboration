b22d0dde6d918118e66b0a9ae2c97879c69677da
AW492 <studentavaniqueweir@gmail.com>
2025-08-12 23:34:44 -0500

    modification to api and script.js to corect CORS error

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app = FastAPI()
 origins = ["https://heathcareserveca.netlify.app", 
            "http://127.0.0.1:5500",
            "http://localhost:5500",
+           "https://aaa-collaboration.onrender.com",
            ]
 
 app.add_middleware(
